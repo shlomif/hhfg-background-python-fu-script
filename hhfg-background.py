728e7aac823a0cbdcc8d5141524930be375aa03c
Shlomi Fish <shlomif@iglu.org.il>
2011-02-27 20:10:37 +0200

    Add hhfg background.

---
+++ hhfg-background.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from gimpfu import *
+import time
+
+gettext.install("gimp20-python", gimp.locale_directory, unicode=True)
+
+def hhfg_background(image_width, image_height, radius, margin_width, margin_height, filename_base):
+
+    gimp.context_push()
+
+    image = gimp.Image(image_width, image_height, RGB)
+    layer = gimp.Layer(image, "HHFG1", image_width, image_height, RGBA_IMAGE, 100.0, NORMAL_MODE)
+    gimp.bucket_fill(layer, BG_BUCKET_FILL, NORMAL_MODE, 100, 0, FALSE, 0, 0);
+    gimp.round_rect_select(image, margin_width, margin_height, image_width-margin_width*2, image_height-margin_width*2, CHANNEL_OP_REPLACE, FALSE, FALSE, 0, 0)
+
+    gimp.bucket_fill(layer, FG_BUCKET_FILL, NORMAL_MODE, 100, 0, FALSE, 0, 0);
+    gimp.Display(image)
+    gimp.context_pop()
+
+register(
+    "python-fu-hhfg-background",
+    N_("Create the Human Hacking Field Guide XHTML Background Image"),
+    "HHFG Background",
+    "Shlomi Fish",
+    "Shlomi Fish",
+    "2011",
+    N_("HHFG Background..."),
+    "RGB*, GRAY*",
+    [
+        (PF_INT, "image_width", "Input Image Width", 1024),
+        (PF_INT, "image_height", "Input Image Height", 768),
+        (PF_INT, "radius", "Input radius of the blur", 50),
+        (PF_INT, "margin_width", "Input margin Width", 100),
+        (PF_INT, "margin_height", "Input margin height", 100),
+        (PF_STRING, "filename_base", "Output filename base", "hhfg-back-"),
+    ],
+    [],
+    hhfg_background,
+    menu="<Image>/File/Create/Patterns",
+    domain=("gimp20-python", gimp.locale_directory)
+    )
+
+main()
